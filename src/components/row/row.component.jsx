1229a977ef7447c0eeafb9ee897fef1e98e6c17e
Chris Kim <chrisdjkim@gmail.com>
2020-09-08 14:22:45 -0400

    Added row-item component that renders data of each movie

--- src/components/row/row.component.jsx
+++ src/components/row/row.component.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import instance from "../../axios";
 
+import RowItem from "../row-item/row-item.component";
+
+const imageBaseUrl = "https://image.tmdb.org/t/p/original";
+
 function Row({ getUrl }) {
   const [shows, setShows] = useState([]);
 
+  //Gets and updates shows object corresponding to getUrl
   useEffect(() => {
     async function getData() {
       const request = await instance.get(getUrl);
@@ -15,7 +20,16 @@ function Row({ getUrl }) {
   return (
     <div className="row">
       This is a row
-      <div className="row__images">This is where images go</div>
+      <div className="row__slider">
+        This is where images go
+        {shows.map((value) => (
+          <RowItem
+            key={value.id}
+            showData={value}
+            imageUrl={`${imageBaseUrl}${value.poster_path}`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
